decb0422448b63f73f03e6c77c26641a2de6db4f
Paul Colusso <paulcolusso@gmail.com>
2023-10-24 15:49:11 +1100

    Use anyhow, add context to errors.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-use std::{process::{Command, Stdio}, error::Error, env, path::Path, fs::{self, File}, io::{BufReader, BufRead, self}};
-use clap::{Parser, Args};
+use anyhow::{Context, Result};
+use clap::{Args, Parser};
+use std::{
+    env,
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+    path::Path,
+    process::{Command, Stdio},
+};
 
 #[derive(Parser)]
 #[clap(bin_name = "tf")]
@@ -8,24 +15,24 @@ enum Commands {
     Plan(Plan),
     Apply(Apply),
     Destroy(Destroy),
-    Init
+    Init,
 }
 
 #[derive(Args)]
 struct SetEnv {
-    new_env: String
+    new_env: String,
 }
 
-fn set_env(opts: SetEnv) -> Result<(), Box<dyn Error>> {
+fn set_env(opts: SetEnv) -> Result<()> {
     // Update .envrc
     let mut new_contents = String::new();
     {
-        let envrc = File::open(".envrc")?;
+        let envrc = File::open(".envrc").context("Cannot open envrc file")?;
         let reader = BufReader::new(envrc);
-        
+
         for line in LineIterator::new(b'\n', reader) {
             let line = line.unwrap();
-            let line = std::str::from_utf8(&line)?;
+            let line = std::str::from_utf8(&line).context("envrc appears to not be valid UTF-8")?;
             if line.contains("export ENV") {
                 // we can use \n, as we're expecting bash anyway.
                 new_contents.push_str(&format!("export ENV={}\n", &opts.new_env));
@@ -34,12 +41,12 @@ fn set_env(opts: SetEnv) -> Result<(), Box<dyn Error>> {
             }
         }
     }
-    fs::write(".envrc", new_contents)?;
+    fs::write(".envrc", new_contents).context("Cannot write envrc file changes")?;
 
     // remove .terraform()
     let terraform_dir = Path::new(".terraform");
     if terraform_dir.exists() {
-        fs::remove_dir_all(terraform_dir)?;
+        fs::remove_dir_all(terraform_dir).context("Unable to remove Terraform cache directory.")?;
     }
 
     println!("Run 'direnv allow' to load new env changes. Terraform will need to be init'd again.");
@@ -50,16 +57,18 @@ fn set_env(opts: SetEnv) -> Result<(), Box<dyn Error>> {
 #[derive(Args)]
 struct Apply {
     #[clap(short = 'y')]
-    auto_approve: bool
+    auto_approve: bool,
 }
 
-fn apply(opts: Apply) -> Result<(), Box<dyn Error>> {
+fn apply(opts: Apply) -> Result<()> {
     check_env()?;
-    let env = env::var("ENV")?;
+    let env = env::var("ENV").context("ENV var not set")?;
     let file = Path::new("envs").join(env).join("main.tfvars");
     let file_str = file.to_string_lossy();
-    let mut args = vec!("destroy", "-var-file", &file_str);
-    if opts.auto_approve { args.push("--auto-approve"); }
+    let mut args = vec!["destroy", "-var-file", &file_str];
+    if opts.auto_approve {
+        args.push("--auto-approve");
+    }
 
     run_terraform(args)?;
 
@@ -71,36 +80,34 @@ struct Plan {
     // additional_args: Option<Vec<String>>,
 }
 
-fn plan() -> Result<(), Box<dyn Error>>{
+fn plan() -> Result<()> {
     check_env()?;
     let env = env::var("ENV").expect("env not set");
     let file = Path::new("envs").join(env).join("main.tfvars");
-    
+
     run_terraform(["plan", "-var-file", &file.to_string_lossy()])?;
 
     Ok(())
 }
 
 #[derive(Args)]
-struct Destroy {
-    
-}
+struct Destroy {}
 
-fn destroy() -> Result<(), Box<dyn Error>> {
+fn destroy() -> Result<()> {
     check_env()?;
     let env = env::var("ENV")?;
     let file = Path::new("envs").join(env).join("main.tfvars");
-    
+
     run_terraform(["destroy", "-var-file", &file.to_string_lossy()])?;
 
     Ok(())
 }
 
-fn init() -> Result<(), Box<dyn Error>> {
+fn init() -> Result<()> {
     check_env()?;
     let env = env::var("ENV")?;
     let file = Path::new("envs").join(env).join("terraform_state.tfvars");
-    
+
     run_terraform(["init", "-backend-config", &file.to_string_lossy()])?;
 
     Ok(())
@@ -108,23 +115,23 @@ fn init() -> Result<(), Box<dyn Error>> {
 
 // Helpers
 
-fn check_env() -> Result<(), Box<dyn Error>> {
+fn check_env() -> Result<()> {
     env::var("ENV")?;
     env::var("AWS_PROFILE")?;
 
     Ok(())
 }
 
-fn run_terraform<'a>(args: impl IntoIterator<Item = &'a str>) -> Result<(), Box<dyn Error>> {
+fn run_terraform<'a>(args: impl IntoIterator<Item = &'a str>) -> Result<()> {
     Command::new("terraform")
         .args(args)
         .stdout(Stdio::inherit())
         .spawn()?
         .wait()?;
 
-        Ok(())
+    Ok(())
 }
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<()> {
     let args = Commands::parse();
 
     match args {
@@ -148,7 +155,6 @@ impl<T: BufRead> LineIterator<T> {
         Self { delimiter, reader }
     }
 }
-
 
 impl<T: BufRead> Iterator for LineIterator<T> {
     type Item = io::Result<Vec<u8>>;
